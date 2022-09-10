3fbd09df94b7e8050d2538e40df6ba16d77a4e85
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-10 14:10:09 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /** 
  * main  - print the string using printf function
  * Return : Always 0 (Success)
@@ -7,6 +7,6 @@ int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return  0);
+	return  (0);
 
 }
