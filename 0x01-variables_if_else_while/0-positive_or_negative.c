69beda5e7c0a31b60c94a2f152a1e7bc98e53674
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-12 11:14:13 -0700

    negative posative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - print the string using put function
  * Return: Always 0 (Success)
  */
@@ -12,12 +12,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
-		printf("%dd is posative\n", n);
-	else if (n== 0)
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else 
-		printf("%d is negative\n, n);
+		printf("%d is negative\n", n);
 
 	return (0);
 
