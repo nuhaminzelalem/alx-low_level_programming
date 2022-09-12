d8fdfd7f94e588d13a09aca1756dbfe844be247d
Nuhamin Zelalem <111012918+nuhaminzelalem@users.noreply.github.com>
2022-09-12 21:40:39 +0300

    Create 5-print_numbers.c
    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
