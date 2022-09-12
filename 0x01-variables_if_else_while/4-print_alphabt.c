fbf08fbfc5a2a24ebaf1633706b7bc875e6a64bc
Nuhamin Zelalem <111012918+nuhaminzelalem@users.noreply.github.com>
2022-09-12 21:38:43 +0300

    Create 4-print_alphabt.c
    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all the letters except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha_low[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (alpha_low[i] != 'e' && alpha_low[i] != 'q')
+		{
+			putchar(alpha_low[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
