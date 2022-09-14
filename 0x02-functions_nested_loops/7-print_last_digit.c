499f99d7d6fd0eb32fc9791b0baf72e59a93545e
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 14:06:10 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @a: last digit of a number
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int)
+
+{
+	char last[] = '12345';
+	{
+		int i=0; 
+		while ( i <=4)
+		{
+			return (last[4]);
+		}
+	}
+	_putchar('\n');
+}
