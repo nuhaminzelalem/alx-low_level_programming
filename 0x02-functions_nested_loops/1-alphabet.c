abb0cf7a62be7d0a7ac7fa822ba31b2c1805c88c
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 10:56:31 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,12 @@ void print_alphabet(void)
 
 {
 	char put[] = "abcdefghijklmnopqrstuvwxyz";
-	
-	for (int i = 0; i < 26; i++)
+int i = 0;	
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(put[i]);
 
 	}
 	_putchar('\n');
 
-	return (0);
 }
