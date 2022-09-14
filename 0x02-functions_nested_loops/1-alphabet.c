710da9689e99b24fc6d716568463791e7a34ccfd
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 10:53:29 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,7 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
+
 {
 	char put[] = "abcdefghijklmnopqrstuvwxyz";
 	
@@ -13,5 +14,6 @@ void print_alphabet(void)
 
 	}
 	_putchar('\n');
+
 	return (0);
 }
