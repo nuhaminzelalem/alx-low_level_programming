b68aa92c24c2fa7158a284dde062ae8f001f6708
Nuhamin Zelalem <111012918+nuhaminzelalem@users.noreply.github.com>
2022-09-15 01:17:02 +0300

    Create 10-add.c
    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integres
+ * @i: parameter
+ * @j: the second operand to be added
+ * Return: Always 0 (Succeed)
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
