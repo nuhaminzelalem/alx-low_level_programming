fc1c7c4f723aef5171ebe1958676516d5da297d5
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 11:53:54 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+/**
+ * int _islower - print 1 if c is lowercase 0 if c is uppercase
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+
+{
+	if ( c >= 97 || c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
