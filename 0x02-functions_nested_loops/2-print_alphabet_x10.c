affd2667992afa01477e1d827f01a2da24819f7f
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 11:28:21 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,21 @@
 void print_alphabet_x10(void)
 
 {
-	char put[]="abcdefghijklmnopqrstuvwxyz";
+	char put[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 	int j = 1;
+
 	for (j = 1; j <= 10; j++)
+	
 	{
 
-			for (i = 0;i < 26;i++)
+			for (i = 0; i < 26; i++)
+			
 			{
 				_putchar(put[i]);
 			}
-
 			_putchar('\n');
-
                            
 	}
-
 }
 
