0e824c9fe5d6e7c12b4f0cd71335da64e0c3cd75
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 12:44:57 -0700

    c is lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - print lower and upper case
+ * @c: c is lower or upper
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c);
+
+{
+	if ((c <= 'a') || (c >= 'z') && (c <='A') || (C >= 'Z'))
+	{
+		return(1);
+	}
+	_putchar('\n');
+}
