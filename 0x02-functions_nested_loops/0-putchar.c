e25f4559d9f7fa645e1668e87c78fda8213b1d04
Nuhamin Zelalem <111012918+nuhaminzelalem@users.noreply.github.com>
2022-09-14 18:24:42 +0300

    Update 0-putchar.c
    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
 #include "main.h"
 /**
- * main -  print _putchar
+ * main - prints _putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char put[9]="_putchar";
+char word[] = "_putchar";
+	int i;
 
-	int i=0;
-
-	while(i < 9)
-
+	for (i = 0; i < 8 ; i++)
 	{
-		_putchar(put[i]);
-
-		_putchar('put[i]');
-
-		i++;
- 
+<<<<<<< HEAD
+		_putchar(word[i]);
+=======
+		_putchar('word[i]');
+>>>>>>> d5147f48088e96f675831df8f2161401247d8f9d
 	}
 	_putchar('\n');
-
 	return (0);
 }
