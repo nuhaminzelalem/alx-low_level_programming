a2cf13e9a60bdf10b64ac1318685505e8cb08679
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 08:43:28 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints "_putchar" followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i=0;
+	char put[]="_putschar";
+	while (i < 9)
+	{
+	_putchar('put[i]');
+	}
+	_putchar('\n');
+	return (0);
+}
