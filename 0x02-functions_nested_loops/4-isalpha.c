1f78587d5e0ac2aa9c5296bc50da12f2bc9094d3
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 12:52:25 -0700

    c is lower or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,11 @@
 int _isalpha(int c)
 
 {
-	if ((c <= 'a') || (c >= 'z') && (c <='A') || (C >= 'Z'))
+	if (((c <= 'a') || (c >= 'z')) && (c <='A') || (c >= 'Z')))
 	{
 		return(1);
 	}
+	else
+		return (0);
 	_putchar('\n');
 }
