b4b969709ada930c782120c0fe359a291f8144cc
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 13:30:06 -0700

    + - 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *  print_sign - print + 0 -
- *  @c: print + 0 -
+ *  @n: print + 0 -
  *  Return: Always 0  (Success)
  */
 int print_sign(int n)
@@ -9,18 +9,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	_putchar('\n');
 }
