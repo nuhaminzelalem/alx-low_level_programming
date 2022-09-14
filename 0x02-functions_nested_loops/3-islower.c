f74ea834374037c077dabe148c9c390695d761e3
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 12:32:27 -0700

    1 if c is lowercase 0 if c is upper

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * int _islower - print 1 if c is lowercase 0 if c is uppercase
+ * _islower - print 1 if c is lowercase 0 if c is uppercase
+ * @c: c may be upper or lower
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 
 {
-	if ( c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
