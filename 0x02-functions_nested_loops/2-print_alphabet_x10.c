00d062a8250d879d046ab2c4eec730e194f1462b
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-14 11:15:43 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - print lowercase alphabet 10 times
+ *  Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+
+{
+	char put[]="abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int j = 1;
+	for (j = 1; j <= 10; j++)
+	{
+
+			for (i = 0;i < 26;i++)
+			{
+				_putchar[i];
+			}
+
+	}
+	_putchar('\n');
+}
+
