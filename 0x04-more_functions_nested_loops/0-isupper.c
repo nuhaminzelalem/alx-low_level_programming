e9751a5f82e4060e1994321aa5bfa411f26b3aab
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-15 10:39:49 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,7 @@
 int _isupper(int c)
 
 {
-	if (c >= 'A') || (c <= 'Z')
+	if (c >= 'A') && (c <= 'Z')
 	{
 		return (1);
 	}
@@ -15,5 +15,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-	_putchar('\n');
 }
