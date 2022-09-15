504b5f82b070f017a9ac04cc6ee5a00623a4c6d5
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-15 12:56:33 -0700

    except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print except 2 and 4
+ * Return: Alwaays 0 (Success)
+ */
+void print_most_numbers(void)
+
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+
+	{
+		if (c != (50 || 52))
+				
+				{
+				_putchar(c);
+				}
+				}
+				_putchar('\n');
+				}
