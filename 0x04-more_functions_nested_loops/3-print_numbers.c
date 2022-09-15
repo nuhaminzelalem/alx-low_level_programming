44a0145d5c5389ebeb2b0fb566badddc132443e9
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-15 12:08:12 -0700

    digit in putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers - iis it digit
+* @c: check wheather it is digit or not
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+		_putchar(1);
+	}
+	else
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
