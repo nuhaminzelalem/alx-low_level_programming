3de7b72e87999d4f03948210f432bb824a9e2dd0
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-15 09:23:41 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - print  uppercase character
+ * @c: check upper case character
+ */
+int _isupper(int c)
+
+{
+	if (c > A) || (c < Z)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
