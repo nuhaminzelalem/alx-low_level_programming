093a8cd3b564ad70e0f1eea8ddb621d5585e2c33
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-15 11:54:36 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * mul - print mul
- * @a: a 
- * @b: b
+ * @a: a please
+ * @b: b please
  * Return: Always 0 (Sucess)
  */
  int mul(int a, int b)
 
 {
 	int mul1;
-	
+
 	mul1 = a * b;
 	return (mul1);
 }
