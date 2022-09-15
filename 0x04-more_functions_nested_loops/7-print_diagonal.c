fd236e69143b7a7d475cc7357140205b685ddc3d
Nuhamin Zelalem <111012918+nuhaminzelalem@users.noreply.github.com>
2022-09-15 23:55:46 +0300

    Create 7-print_diagonal.c
    dialog

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+include "main.h"
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal.
+ * @n: input parameter
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i != j)
+					_putchar(' ');
+				else
+				{
+					_putchar(92);
+					_putchar('\n');
+					break;
+				}
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
