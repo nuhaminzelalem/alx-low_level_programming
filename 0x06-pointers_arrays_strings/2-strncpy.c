3a41739052fee01882fe83fc7a2c2e08adccc7ab
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-22 12:18:23 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - string copy
+ * @dest: destnation
+ * @src: source
+ * @n: number
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (src [i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (n > i)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
