5f7b91a491e959cdbcb43c4805f5d5d211eadbd0
Nuhamin <nuhaminzelalem1228@gmail.com>
2022-09-28 10:18:24 -0700

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_recursion - recursion
+ * @s: string
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+
+{
+	int i;
+
+	i = 0;
+	printf("%s" , s);
+	printf("\n\n");
+}
